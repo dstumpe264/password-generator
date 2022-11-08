735a0dabb8472cd51f86bd4bd5fec52ed8582357
David Stumpe <dstumpe264@gmail.com>
2022-11-08 17:06:53 -0500

    added criterias to readme, created conditional statements for password length

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Password Generator
+
+## Criteria
+
+* generate password button 
+* series of prompts for password criteria
+* selection of criteria to include in the password
+* password length
+* between 8 and 128 characters
+* 
--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+    // ask user how many characters they want to use 
+    var length = parseInt(prompt('How many characters?'), 10);
+    
+    // make sure user enters a number
+    if (Number.isNaN(length)){
+        alert('Length must be a number');
+        return null;
+    }
+    // make sure length is between 8 and 128
+    if (length < 8 || length > 128){
+        alert('Length must be more than 8 and less than or equal to 128.');
+        return null;
+    }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
